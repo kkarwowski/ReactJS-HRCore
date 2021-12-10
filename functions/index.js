e8d9235df297caff3fe2a1f6c16c4d6357a8588d
royallon@outlook.com <crypto_sarge@icloud.com>
2021-12-10 11:37:08 +0000

    cloud functions

---
+++ functions/index.js
@@ -0,0 +1,60 @@
+const functions = require("firebase-functions");
+const admin = require("firebase-admin");
+
+admin.initializeApp()
+// // Create and Deploy Your First Cloud Functions
+// // https://firebase.google.com/docs/functions/write-firebase-functions
+//
+// exports.helloWorld = functions.https.onRequest((request, response) => {
+//   functions.logger.info("Hello logs!", {structuredData: true});
+//   response.send("Hello from Firebase!");
+// });
+
+
+exports.getAllEmployedScheaduled = functions.pubsub.schedule('0 0 1 * *')
+  .timeZone('Europe/London') // Users can choose timezone - default is America/Los_Angeles
+  .onRun((context) => {
+    const associatesRef = admin.firestore().collection("Associates")
+    const totalAssociatesRef = admin.firestore().collection("TotalAssociatesChart")
+    let total = 0
+
+    associatesRef.where("EmplStatus","==","Employed").get().then(querySnapshot => {
+        querySnapshot.forEach(doc => {
+            total +=1
+        })
+    }).finally(() => {
+        totalAssociatesRef.doc().set({
+        Date: admin.firestore.Timestamp.now(),
+        Total: Number(total)
+    })
+    })
+  return null;
+});
+
+// exports.getAllEmployedScheaduled = functions.https.onRequest((request, response) => {
+   
+//     const associatesRef = admin.firestore().collection("Associates")
+//     const totalAssociatesRef = admin.firestore().collection("TotalAssociatesChart")
+//     let total = 0
+
+//     associatesRef.where("EmplStatus","==","Employed").get().then(querySnapshot => {
+//         querySnapshot.forEach(doc => {
+//             total +=1
+//         })
+//     }).finally(() => {
+//         totalAssociatesRef.doc().set({
+//         Date: admin.firestore.Timestamp.now(),
+//         Total: Number(total)
+//     })
+//     })
+    
+//   });
+// totalAssociatesRef.doc().set({
+    //     Date: admin.firestore.Timestamp.now(),
+    //     Total: Number("25")
+    // })
+    // associatesRef.doc().set({
+    //     FirstName:"Markkkk"
+    // })
+ 
+    // functions.logger.info("total",qq, {structuredData: true});

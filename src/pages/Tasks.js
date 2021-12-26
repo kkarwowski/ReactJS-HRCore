c6a0e26b2fde3391513e286171f23d58c386fe6d
kkarwowski <crypto_sarge@icloud.com>
2021-12-26 18:07:52 +0000

    adding modal to Add New Task

--- src/pages/Tasks.js
+++ src/pages/Tasks.js
@@ -1,7 +1,9 @@
 import Page from "../components/Page";
 import AddTask from "../components/Tasks/AddTask";
-import { useTheme } from "@mui/material/styles";
+import { useTheme, styled, alpha } from "@mui/material/styles";
 import { useEffect, useState, useContext } from "react";
+import PaidIcon from "@mui/icons-material/Paid";
+import ManageAccountsIcon from "@mui/icons-material/ManageAccounts";
 import moment from "moment";
 
 import {
@@ -18,7 +20,21 @@ import {
   update,
 } from "firebase/database";
 // import { rtdb } from "../utils/firebase";
-import { Button, Grid, Card, Box, Typography, Stack } from "@mui/material";
+import {
+  Button,
+  Grid,
+  Card,
+  Box,
+  Typography,
+  Stack,
+  Menu,
+  MenuItem,
+  Backdrop,
+  Modal,
+  Fade,
+} from "@mui/material";
+import PropTypes from "prop-types";
+
 import {
   associatesContext,
   tasksToApproveContext,
@@ -28,9 +44,17 @@ import { getDoc, doc } from "firebase/firestore";
 import { db } from "../utils/firebase";
 import TaskCard from "../components/Tasks/TaskCard";
 import AddCircleIcon from "@mui/icons-material/AddCircle";
+import { Controller, useForm } from "react-hook-form";
+
 const MyTasks = () => {
   const theme = useTheme();
   const { userData } = useAuth();
+  const [open, setOpen] = useState(false);
+  const [anchorEl, setAnchorEl] = useState(null);
+  const openMenu = Boolean(anchorEl);
+  const [PopupOpen, setPopupOpen] = useState(false);
+  const { handleSubmit, reset, control } = useForm();
+
   const { associates } = useContext(associatesContext);
   const [userDetails, setUserDetails] = useState();
   const [myManager, setMyManager] = useState();
@@ -101,12 +125,154 @@ const MyTasks = () => {
   const rejectedTasks = filterObject(tasks, "status", "rejected");
 
   const completeTasks = { ...approvedTasks, ...rejectedTasks };
+  const handleClickAction = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleCloseAction = () => {
+    setAnchorEl(null);
+  };
+  const handleClickOpen = () => {
+    setOpen(true);
+    handleCloseAction();
+  };
+  const onSubmit = (data) => uploadFile(data);
+  const handleClose = () => {
+    setPopupOpen(false);
+  };
 
+  const StyledMenu = styled((props) => (
+    <Menu
+      elevation={0}
+      anchorOrigin={{
+        vertical: "bottom",
+        horizontal: "right",
+      }}
+      transformOrigin={{
+        vertical: "top",
+        horizontal: "right",
+      }}
+      {...props}
+    />
+  ))(({ theme }) => ({
+    "& .MuiPaper-root": {
+      borderRadius: 6,
+      marginTop: theme.spacing(1),
+      minWidth: 180,
+      color:
+        theme.palette.mode === "light"
+          ? "rgb(55, 65, 81)"
+          : theme.palette.grey[300],
+      boxShadow:
+        "rgb(255, 255, 255) 0px 0px 0px 0px, rgba(0, 0, 0, 0.05) 0px 0px 0px 1px, rgba(0, 0, 0, 0.1) 0px 10px 15px -3px, rgba(0, 0, 0, 0.05) 0px 4px 6px -2px",
+      "& .MuiMenu-list": {
+        padding: "4px 0",
+      },
+      "& .MuiMenuItem-root": {
+        "& .MuiSvgIcon-root": {
+          fontSize: 18,
+          color: theme.palette.text.secondary,
+          marginRight: theme.spacing(1.5),
+        },
+        "&:active": {
+          backgroundColor: alpha(
+            theme.palette.primary.main,
+            theme.palette.action.selectedOpacity
+          ),
+        },
+      },
+    },
+  }));
+
+  const style = {
+    position: "absolute",
+    top: "40%",
+    left: "50%",
+    transform: "translate(-50%, -50%)",
+    width: "90vw",
+    height: "60vh",
+    bgcolor: "background.paper",
+    border: "2px solid #000",
+    boxShadow: 20,
+    p: 4,
+  };
   return (
     <Page title="HR Core - Tasks">
-      <Stack direction="row" justifyContent="flex-end" px={2}>
-        <AddCircleIcon color="primary" fontSize="large" />
-      </Stack>
+      <Modal
+        aria-labelledby="transition-modal-title"
+        aria-describedby="transition-modal-description"
+        open={PopupOpen}
+        onClose={handleClose}
+        closeAfterTransition
+        BackdropComponent={Backdrop}
+        BackdropProps={{
+          timeout: 500,
+        }}
+      >
+        <Fade in={PopupOpen}>
+          <Box sx={style}>
+            <form>
+              <Grid
+                container
+                direction="column"
+                alignContent="center"
+                alignItems="center"
+                rowSpacing={3}
+              >
+                <Grid item>
+                  <Typography>
+                    Please select category of uploaded file:
+                  </Typography>
+                </Grid>
+
+                <Grid item></Grid>
+
+                <Grid item>
+                  <Button onClick={handleSubmit(onSubmit)} variant="contained">
+                    Submit
+                  </Button>
+                </Grid>
+
+                {/* <Button onClick={() => reset()} variant={"outlined"}> */}
+                {/* Reset */}
+                {/* </Button> */}
+              </Grid>
+            </form>
+          </Box>
+        </Fade>
+      </Modal>
+      <Grid container direction="row" justifyContent="flex-end">
+        <Grid item>
+          <Button
+            variant="contained"
+            aria-haspopup="true"
+            aria-expanded={open ? "true" : undefined}
+            onClick={handleClickAction}
+            endIcon={<AddCircleIcon color="white" fontSize="large" />}
+          >
+            Add Task
+          </Button>
+
+          <StyledMenu
+            id="demo-customized-menu"
+            MenuListProps={{
+              "aria-labelledby": "demo-customized-button",
+            }}
+            anchorEl={anchorEl}
+            open={openMenu}
+            onClose={handleCloseAction}
+          >
+            <MenuItem onClick={handleClickOpen} disableRipple>
+              <PaidIcon sx={{ color: "#ff0000" }} />
+              Salary Increase
+            </MenuItem>
+            <MenuItem onClick={() => handleClickOpen()} disableRipple>
+              <ManageAccountsIcon color={"#ff0000"} />
+              Title Change
+            </MenuItem>
+          </StyledMenu>
+        </Grid>
+      </Grid>
+
       <Grid container direction="row" sx={{ p: 2 }} spacing={2} rowSpacing={2}>
         <Grid item xs={12} md={4} lg={4}>
           <Grid container direction="column" spacing={2}>

c35b660ff51537957b2c98950788ac65b6384105
royallon@outlook.com <crypto_sarge@icloud.com>
2022-03-07 20:54:57 +0000

    pagination

--- src/pages/Thanks.js
+++ src/pages/Thanks.js
@@ -3,10 +3,11 @@ import {
   Grid,
   Switch,
   FormControlLabel,
-  Card,
   Button,
   Box,
   CircularProgress,
+  TextField,
+  MenuItem,
 } from "@mui/material";
 import {
   ref,
@@ -14,9 +15,11 @@ import {
   getDatabase,
   limitToFirst,
   query,
-  orderByKey,
-  orderByValue,
   orderByChild,
+  startAfter,
+  startAt,
+  endAt,
+  limitToLast,
 } from "firebase/database";
 import { Link } from "react-router-dom";
 
@@ -28,15 +31,19 @@ const Thanks = () => {
   const { userData } = useAuth();
   const [thanks, setThanks] = useState();
   const [filterID, setFilterID] = useState();
+  const [pagination, setPagination] = useState(3);
+  const paginationChoices = [3, 10, 20, 30, 40];
   useEffect(() => {
     const dbrt = getDatabase();
+    const monthAgo = new Date().setDate(new Date().getDate() - 30);
     // const AllNotifications = query(ref(dbrt, `Notifications/${userData.id}`));
     const AllThanks = query(
       ref(dbrt, "Thanks"),
-      // orderByChild("Category"),
-      limitToFirst(20)
+      orderByChild("Timestamp"),
+      startAt(monthAgo),
+      limitToLast(pagination)
+      // endAt("TeamPlayer")
     );
-    console.log(AllThanks);
     onValue(AllThanks, (snapshot) => {
       if (snapshot.val() != null) {
         console.log("snap", snapshot);
@@ -52,11 +59,7 @@ const Thanks = () => {
       } else {
       }
     });
-  }, []);
-
-  const setID = (id) => {
-    setFilterID(id);
-  };
+  }, [pagination]);
 
   const filteredThanks = (array, id) => {
     if (id) {
@@ -71,43 +74,81 @@ const Thanks = () => {
   return (
     <>
       <Page title="HR Core - Thanks">
-        <Grid
-          container
-          direction="row"
-          sx={{ p: 1 }}
-          justifyContent="space-between"
+        <Box
+          sx={{
+            px: 1,
+            py: 0.5,
+            mb: 2,
+            borderRadius: "10px",
+            boxShadow: 7,
+            background: "white",
+            borderBottom: "solid black 3px",
+          }}
         >
-          <Grid item>
-            <Card sx={{ paddingLeft: 1, paddingRight: 1 }}>
-              <FormControlLabel
-                control={
-                  <Switch
-                    onChange={(e) => {
-                      if (e.target.checked === true) {
-                        setFilterID(userData.id);
-                      } else {
-                        setFilterID(undefined);
-                      }
-                    }}
+          <Grid
+            container
+            direction="row"
+            sx={{ p: 1 }}
+            justifyContent="space-between"
+            alignContent="center"
+            alignItems="center"
+          >
+            <Grid item>
+              <Grid
+                container
+                direction="row"
+                columnSpacing={2}
+                justifyContent="flex-start"
+                alignItems="center"
+              >
+                <Grid item lg={6}>
+                  <FormControlLabel
+                    control={
+                      <Switch
+                        onChange={(e) => {
+                          if (e.target.checked === true) {
+                            setFilterID(userData.id);
+                          } else {
+                            setFilterID(undefined);
+                          }
+                        }}
+                      />
+                    }
+                    label="My Thanks"
                   />
-                }
-                label="Received Thanks"
-              />
-            </Card>
+                </Grid>
+                <Grid item lg={6}>
+                  <TextField
+                    select
+                    fullWidth
+                    label="Per Page"
+                    value={pagination}
+                    size="small"
+                    onChange={(e) => setPagination(e.target.value)}
+                  >
+                    {paginationChoices.map((pad) => (
+                      <MenuItem value={pad} key={pad}>
+                        {pad}
+                      </MenuItem>
+                    ))}
+                  </TextField>
+                </Grid>
+              </Grid>
+            </Grid>
+            <Grid item>
+              <Button variant="contained" component={Link} to={"givethanks"}>
+                Give Thanks
+              </Button>
+            </Grid>
           </Grid>
-          <Grid item>
-            <Button variant="contained" component={Link} to={"givethanks"}>
-              Give Thanks
-            </Button>
-          </Grid>
-        </Grid>
+        </Box>
+
         <Grid
           container
           direction="row"
           columnSpacing={1}
           rowSpacing={1}
           alignItems="center"
-          sx={{ p: 1 }}
         >
           {thanks && filteredThanks && userData ? (
             (console.log(filteredThanks(thanks, filterID)),
